9c04496d99899d5d696641425fcb28a405d67a72
Ranim Tom <ranim.tom@unabiz.com>
2024-07-09 13:32:54 +0200

    A demo about system call: write().
    A simple project to illustrate the usage of some system call functions, such as write().

---
+++ system_call_demo/main.c
@@ -0,0 +1,30 @@
+
+
+#include <stdio.h>
+#include <string.h>
+
+#include <unistd.h> // for linux processes
+
+int main(){
+
+	printf("\n Welcome to system programming in C! \n");
+
+	// Declaring and initializing some variables
+	int msg_len = 0;
+
+	char buff[100];
+
+	// copying string into buff
+	strcpy(buff,"some example of system call using write()");
+
+	// taking the actual length
+	msg_len = strlen(buff);
+
+	// printing to the screen using write() system call function
+	size_t sentence = write(1,buff,msg_len);
+	// 1 -> to stout (the screen)
+
+
+
+
+} // End main()
