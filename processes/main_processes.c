500ca9b04c0b7fd8ead5acb6b26db0da05230923
Ranim Tom <ranim.tom@unabiz.com>
2024-08-23 16:57:18 +0200

    adding wait demo
    - an example about wait() using printing some numbers.
    - don't forget to put an argument to wait(): NULL can be used if we are not intersted by the status of the terminated process.
    - Concept of processes: id of child always = 0
    - if id !=0 -> this means we are in the parent process
    - if we need to use some variable by the 2 processes, we need to declare outside the block of if (id ==0 ) or if (id !=0)
    like the n variable, so it can be visible by the for_loop().

--- processes/main_processes.c
+++ processes/main_processes.c
@@ -4,15 +4,16 @@
 #include <stdio.h>
 
 #include "fork_demo.h"
+#include "wait_demo.h"
 
-#define DEMO_CHOICE 0
+#define DEMO_CHOICE 1
 
 int main(){
 
 	printf("\n Welcome to processes in C! \n");
 
 	int demo_list_choice[] = {0, // fork_demo
-		1,
+		1, // wait_demo
 		2,
 		3};
 
@@ -22,6 +23,10 @@ int main(){
 
 		case 0:
 			run_fork();
+			break;
+
+		case 1:
+			run_wait();
 			break;
 
 		default:
---
+++ processes/wait_demo.c
@@ -0,0 +1,72 @@
+
+
+
+
+/*
+ * Task: write a program that print from 1 ->10 (1 by 1)
+ *
+ * From 1->5, it should be print by the child process
+ *
+ * From 6->10; it should be print by the main process
+ *
+ * */
+
+
+#include "wait_demo.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+
+void run_wait(void){
+
+	printf("\n \t -> Starting wait demo \n \n");
+
+	// creating a child process.
+
+	pid_t id = fork();
+
+	// it is necessary to declare the variable n here
+	// so it can be common and used by the 2 processes (parent and the child)
+	int n;
+
+
+	if(id == 0){
+		// here we are in the child
+		n = 1;
+
+	}else{
+		// since we have only 1 fork() -> so in total we have 2 process
+		// here we are in the main process
+		n = 6;
+	}
+
+	if(id != 0){
+		// id not 0 -> definitely not a child process
+		// here we are in the parent process
+
+		wait(NULL); // -> we are waiting for the child process to finish its execution
+		// we use NULL as argument because I am not interested by the status of
+		// the child process when it is finished
+	}
+
+	for(int i = n; i< n+5 ; i++){
+
+		printf("%d",i);
+
+	} // End for
+
+	if(id != 0){
+		printf("\n");
+
+		// we let a blank line only once
+		// this why we write it in the parent process
+		// Also the child will print the upper part
+
+	}
+
+
+} // End run_wait()
